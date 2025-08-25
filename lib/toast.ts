7301f84737c5a5fba956d2619aabe2d55beaa72c
Iroegbu David <iroegbu.dgit@gmail.com>
2025-08-25 01:45:18 +0100

    new commit

--- lib/toast.ts
+++ lib/toast.ts
@@ -8,9 +8,12 @@ export function toast_msg({title, description, type}:{title: string, description
                 onClick: () => {},
         },style:{
             fontFamily: "Montserrat",
+            font: '13px',
             color: `${type === 'danger'? '#FF3333': ''}`,
-            background: `${type === 'danger' ? '#FFF5F5': '#ffffff'}`,
-            border: `none`
+            background: `${type === 'danger' ? '#ffffff': '#ffffff'}`,
+            border: `none`,
+            display: 'flex',
+            alignItems: 'end'
         },actionButtonStyle:{
             background: '#FF3333',
             
